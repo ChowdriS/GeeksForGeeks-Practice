6c977ae70d39771e3d3ca222a048b56154bf9c2b
ChowdriS <114232036+ChowdriS@users.noreply.github.com>
2023-12-13 14:40:05 +0530

    13-dec

---
+++ Daily Challenges/2023/12 DEC/13-12-2023-consecutive_ones_not_allowed.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+class Solution{
+public:
+	ll countStrings(int n) {
+	    vector<vector<ll>> dp(n + 1, vector<ll> (2, 0));
+	    const ll mod = 1e9 + 7;
+	    dp[n][0] = dp[n][1] = 1;
+	    
+	    for(int i = n - 1; i > -1; i--){
+	        dp[i][1] = dp[i + 1][0];
+	        
+	        dp[i][0] = (dp[i + 1][1] + dp[i + 1][0]) % mod;
+	    }
+	    
+	    return dp[0][0];
+	}
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
