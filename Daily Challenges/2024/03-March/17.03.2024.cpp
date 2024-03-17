48159473346144eae6c3dceaaca9f62b7c941c12
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2024-03-17 14:06:28 +0530

    17-mar

---
+++ Daily Challenges/2024/03-March/17.03.2024.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** headRef, struct Node** tailRef, int newData) {
+    struct Node* new_node = new Node(newData);
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+    }
+
+    else {
+        (*tailRef)->next = new_node;
+    }
+    *tailRef = new_node;
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+  public:
+    int countPairs(struct Node* head1, struct Node* head2, int x) {
+        unordered_map<int,int> mp;
+        Node* temp = head1;
+        Node* temp1 = head2;
+        while(temp!=nullptr){
+            mp[temp->data]++;
+            temp = temp -> next;
+        }
+        int cnt = 0;
+        
+        while(temp1!=nullptr){
+            if(mp.find( x - temp1->data) != mp.end()) {
+                cnt++;
+            }
+            temp1 = temp1->next;
+        }
+        return cnt;
+    }
+};
+
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        struct Node* head1 = NULL;
+        struct Node* tail1 = NULL;
+        struct Node* tail2 = NULL;
+        struct Node* head2 = NULL;
+        int n1, n2, tmp, x;
+        cin >> n1;
+        while (n1--) {
+            cin >> tmp;
+            append(&head1, &tail1, tmp);
+        }
+        cin >> n2;
+        while (n2--) {
+            cin >> tmp;
+            append(&head2, &tail2, tmp);
+        }
+        cin >> x;
+        Solution obj;
+        cout << obj.countPairs(head1, head2, x) << '\n';
+    }
+    return 0;
+}
