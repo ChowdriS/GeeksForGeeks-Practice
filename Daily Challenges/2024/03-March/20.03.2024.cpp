88d1a9f836c083f504a2adb25e736212025dca09
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2024-03-20 11:13:31 +0530

    20-mar

---
+++ Daily Challenges/2024/03-March/20.03.2024.cpp
@@ -0,0 +1,152 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    Node *root = new Node(stoi(ip[0]));
+
+    queue<Node *> queue;
+    queue.push(root);
+
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        Node *currNode = queue.front();
+        queue.pop();
+
+        string currVal = ip[i];
+
+        if (currVal != "N")
+        {
+
+            currNode->left = new Node(stoi(currVal));
+
+            queue.push(currNode->left);
+        }
+
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        if (currVal != "N")
+        {
+
+            currNode->right = new Node(stoi(currVal));
+
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+/*
+structure of the node of the binary tree is as
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution
+{
+    int ans = 0;
+    int count = 0;
+private:
+    void solve(Node* root,int level=0,int summer = 0){
+        if(!root)   return ;
+        summer += root->data;
+        level++;
+        int left = INT_MIN,right = INT_MIN;
+        if(!root->left and !root->right) {
+            if(count<level){
+                count = level;
+                ans = summer;
+            }
+            else if(count==level){
+                ans = max(summer,ans);
+            }
+            return;
+        }
+        if(root->left){
+            solve(root->left,level,summer);
+        }
+        if(root->right){
+            solve(root->right,level,summer);
+        }
+    }
+public:
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        solve(root,0,0);
+        return ans;
+        
+    }
+};
+
+
+
+int main()
+{
+
+    int t;
+    scanf("%d", &t);
+    cin.ignore();
+    while (t--)
+    {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution obj;
+        int res = obj.sumOfLongRootToLeafPath(root);
+        cout << res << "\n";
+    }
+    return 0;
+}
