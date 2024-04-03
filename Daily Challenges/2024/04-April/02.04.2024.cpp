4607a64f2a6d16f274ffb36a34dd3fcf541d132e
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2024-04-03 08:51:42 +0530

    04-apl

---
+++ Daily Challenges/2024/04-April/02.04.2024.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *left;
+  struct Node *right;
+};
+Node *newNode(int val)
+{
+  Node *temp = new Node;
+  temp->data = val;
+  temp->left = NULL;
+  temp->right = NULL;
+
+  return temp;
+}
+Node *buildTree(string str)
+{
+  if (str.length() == 0 || str[0] == 'N')
+    return NULL;
+
+  vector<string> ip;
+
+  istringstream iss(str);
+  for (string str; iss >> str;)
+    ip.push_back(str);
+
+
+  queue<Node *> queue;
+  queue.push(root);
+
+  int i = 1;
+  while (!queue.empty() && i < ip.size())
+  {
+
+    Node *currNode = queue.front();
+    queue.pop();
+
+    string currVal = ip[i];
+
+    if (currVal != "N")
+    {
+
+      currNode->left = newNode(stoi(currVal));
+
+      queue.push(currNode->left);
+    }
+
+    i++;
+    if (i >= ip.size())
+      break;
+    currVal = ip[i];
+
+    if (currVal != "N")
+    {
+
+      currNode->right = newNode(stoi(currVal));
+
+      queue.push(currNode->right);
+    }
+    i++;
+  }
+
+  return root;
+}
+
+
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+};
+*/
+
+class Solution
+{
+    private:
+    void inorder(Node* root,vector<int>& ans){
+        if(!root)   return;
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+    }
+    public:
+    int absolute_diff(Node *root)
+    {
+        vector<int> ans;
+        inorder(root,ans);
+        int n = ans.size();
+        int result = INT_MAX;
+        for(int i =1;i<n;i++) {
+            result = min(ans[i]-ans[i-1],result);
+        }
+        return result;
+    }
+};
+
+
+int main()
+{
+
+  int t;
+  scanf("%d ", &t);
+  while (t--)
+  {
+    string s;
+    getline(cin, s);
+    Node *root = buildTree(s);
+    if (root == NULL)
+      continue;
+    if (root->left == NULL and root->right == NULL)
+      continue;
+      Solution ob;
+    cout << ob.absolute_diff(root) << '\n';
+  }
+  return 1;
+}
