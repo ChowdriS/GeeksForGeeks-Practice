5be4f3ff69ce3539a8467b3e15a98a0487bbd255
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2024-05-03 20:19:49 +0530

    03-may

---
+++ Daily Challenges/2024/05-May/03.05.2024.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    Node* root = newNode(stoi(ip[0]));
+        
+    queue<Node*> queue;
+    queue.push(root);
+        
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        string currVal = ip[i];
+            
+        if(currVal != "N") {
+                
+            currNode->left = newNode(stoi(currVal));
+                
+            queue.push(currNode->left);
+        }
+            
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        if(currVal != "N") {
+                
+            currNode->right = newNode(stoi(currVal));
+                
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+class Solution
+{
+    public:
+    vector<int> Kdistance(struct Node *root, int k)
+    {
+        queue<Node*> qp;
+        int level = 0;
+        qp.push(root);
+        while(!qp.empty()){
+            int n = qp.size();
+            vector<int> temp;
+            for(int i=0;i<n;i++){
+                Node* node = qp.front();
+                qp.pop();
+                temp.push_back(node->data);
+                if(node->left)  qp.push(node->left);
+                if(node->right) qp.push(node->right);
+            }
+            if(level == k)  return temp;
+            level++;
+        }
+        return {};
+    }
+};
+
+
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        int k;
+		scanf("%d ",&k);
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        Solution obj;
+        vector<int> vec = obj.Kdistance(root, k);
+        for(int i = 0;i<vec.size();i++){
+            cout<<vec[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 1;
+}
