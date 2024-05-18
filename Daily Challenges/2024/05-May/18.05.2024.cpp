c0be9869c3990a09e54ac10175074e7c3d4312ea
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2024-05-18 19:49:32 +0530

    may-18

---
+++ Daily Challenges/2024/05-May/18.05.2024.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        return *max_element(a.begin(),a.end());
+    }
+};
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
