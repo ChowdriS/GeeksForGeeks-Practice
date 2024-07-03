a49b0dee955cba96c6b61df51c51afb88c7ed481
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2024-07-03 16:20:54 +0530

    01-jul

---
+++ Daily Challenges/2024/07-July/01.07.2024.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void push(struct Node **head_ref, int new_data) {
+
+    struct Node *new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void lvl(TreeNode *r) {
+    if (r == NULL)
+        return;
+    queue<TreeNode *> q;
+    q.push(r);
+    while (!q.empty()) {
+        TreeNode *j = q.front();
+        cout << j->data << " ";
+        q.pop();
+        if (j->left)
+            q.push(j->left);
+        if (j->right)
+            q.push(j->right);
+    }
+}
+
+void reverses(Node **head) {
+    Node *prev = NULL;
+    Node *cur = *head;
+    Node *nxt;
+    while (cur != NULL) {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    *head = prev;
+}
+
+
+class Solution {
+  public:
+    void convert(Node *head, TreeNode *&root) {
+        vector<int> nums;
+        Node* temp = head;
+        while(temp!=nullptr){
+            nums.push_back(temp->data);
+            temp = temp->next;
+        }
+        
+        queue<TreeNode*> qp;
+        root = new TreeNode(nums[0]);
+        qp.push(root);
+        
+        int n = nums.size();
+        int ind = 1;
+        
+        while(ind < n){
+            
+            TreeNode* node = qp.front();
+            qp.pop();
+            
+            if(ind < n){
+                node->left = new TreeNode(nums[ind++]);
+                qp.push(node->left);
+            }
+            
+            if(ind < n){
+                node->right = new TreeNode(nums[ind++]);
+                qp.push(node->right);
+            }
+        }
+    }
+};
+
+int main() {
+    int T, i, n, l, k;
+
+    cin >> T;
+
+    while (T--) {
+        struct Node *head = NULL;
+
+        cin >> n;
+        for (i = 1; i <= n; i++) {
+            cin >> l;
+            push(&head, l);
+        }
+        reverses(&head);
+        Node *prevhead = head;
+        TreeNode *root = NULL;
+        Solution obj;
+
+        obj.convert(head, root);
+        if (root == NULL) {
+            cout << "-1\n";
+        } else if (root == NULL && prevhead != NULL) {
+            cout << "-1\n";
+        } else {
+            lvl(root);
+            cout << endl;
+        }
+        // inorder(root);
+        getchar();
+    }
+    return 0;
+}
